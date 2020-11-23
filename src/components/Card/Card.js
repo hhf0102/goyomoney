e7f2550f55ccd78683fa235cd0696385bbe4671d
Edward <edwardhhf0102@gmail.com>
2020-11-23 23:32:26 +0800

    feat: put images into the local folder instead of fetching from URL

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,11 +4,9 @@ import styles from './Card.module.scss';
 
 const Card = ({ imgSrc, isShowed, onClick }) => {
   return (
-    <div
-      className={styles['card']}
-      style={{ backgroundImage: isShowed ? `url(${imgSrc})` : 'none' }}
-      onClick={onClick}
-    ></div>
+    <div className={styles['card']} onClick={onClick}>
+      <img src={isShowed ? imgSrc : null} />
+    </div>
   );
 };
 
--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 
 import styles from './GameBoard.module.scss';
 import Card from '../Card/Card';
+import card_1 from '../../assets/images/card_1.jpg';
+import card_2 from '../../assets/images/card_2.jpg';
+import card_3 from '../../assets/images/card_3.jpg';
+import card_4 from '../../assets/images/card_4.jpg';
+import card_5 from '../../assets/images/card_5.jpg';
+import card_6 from '../../assets/images/card_6.jpg';
+import card_7 from '../../assets/images/card_7.jpg';
+import card_8 from '../../assets/images/card_8.jpg';
+import card_9 from '../../assets/images/card_9.jpg';
+import card_10 from '../../assets/images/card_10.jpg';
 
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -11,19 +21,31 @@ function shuffle(array) {
   return array;
 }
 
-let imageList = [];
+const imageList = [
+  card_1,
+  card_2,
+  card_3,
+  card_4,
+  card_5,
+  card_6,
+  card_7,
+  card_8,
+  card_9,
+  card_10,
+];
+
+let imageListDouble = [];
 
 for (let i = 0; i < 2; i++) {
-  const list = [...Array(10)].map((_, idx) => ({
+  const list = imageList.map((img) => ({
+    imgSrc: img,
     isShowed: false,
-    imgSrc: `https://picsum.photos/id/${idx + 11}/100/120`,
   }));
-  imageList.push(list);
+  imageListDouble = imageListDouble.concat(list);
 }
 
 const GameBoard = () => {
-  const [cardList, setCardList] = useState(shuffle(imageList.flat()));
-  console.log(cardList);
+  const [cardList, setCardList] = useState(shuffle(imageListDouble));
 
   const handleClickCard = (idx) => {
     const newCardList = [...cardList];
