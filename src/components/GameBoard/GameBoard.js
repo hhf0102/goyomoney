16bca963a369c2de0f51670036835038a0604d25
Edward <edwardhhf0102@gmail.com>
2020-11-24 15:25:55 +0800

    refactor: extract some code and put them into the custom hook

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -2,51 +2,12 @@ import React, { useState, useRef, useEffect } from 'react';
 
 import styles from './GameBoard.module.scss';
 import Card from '../Card/Card';
-import card_1 from '../../assets/images/card_1.jpg';
-import card_2 from '../../assets/images/card_2.jpg';
-import card_3 from '../../assets/images/card_3.jpg';
-import card_4 from '../../assets/images/card_4.jpg';
-import card_5 from '../../assets/images/card_5.jpg';
-import card_6 from '../../assets/images/card_6.jpg';
-import card_7 from '../../assets/images/card_7.jpg';
-import card_8 from '../../assets/images/card_8.jpg';
-import card_9 from '../../assets/images/card_9.jpg';
-import card_10 from '../../assets/images/card_10.jpg';
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-const imageList = [
-  card_1,
-  card_2,
-  card_3,
-  card_4,
-  card_5,
-  card_6,
-  card_7,
-  card_8,
-  card_9,
-  card_10,
-];
+import useCardList from './useCardList';
 
 let timerID = null;
 
 const GameBoard = () => {
-  let imageListDouble = [];
-
-  for (let i = 0; i < 2; i++) {
-    const list = imageList.map((img) => ({
-      imgSrc: img,
-      isShowed: false,
-    }));
-    imageListDouble = imageListDouble.concat(list);
-  }
-  const [cardList, setCardList] = useState(shuffle(imageListDouble));
+  const [cardList, setCardList] = useState(useCardList());
   const [isLockBoard, setIsLockBoard] = useState(false);
   const firstCardRefObj = useRef(null);
   const secondCardRefObj = useRef(null);
---
+++ src/components/GameBoard/useCardList.js
@@ -0,0 +1,47 @@
+import card_1 from '../../assets/images/card_1.jpg';
+import card_2 from '../../assets/images/card_2.jpg';
+import card_3 from '../../assets/images/card_3.jpg';
+import card_4 from '../../assets/images/card_4.jpg';
+import card_5 from '../../assets/images/card_5.jpg';
+import card_6 from '../../assets/images/card_6.jpg';
+import card_7 from '../../assets/images/card_7.jpg';
+import card_8 from '../../assets/images/card_8.jpg';
+import card_9 from '../../assets/images/card_9.jpg';
+import card_10 from '../../assets/images/card_10.jpg';
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const useCardList = () => {
+  const imageList = [
+    card_1,
+    card_2,
+    card_3,
+    card_4,
+    card_5,
+    card_6,
+    card_7,
+    card_8,
+    card_9,
+    card_10,
+  ];
+
+  let imageListDouble = [];
+
+  for (let i = 0; i < 2; i++) {
+    const list = imageList.map((img) => ({
+      imgSrc: img,
+      isShowed: false,
+    }));
+    imageListDouble = imageListDouble.concat(list);
+  }
+
+  return shuffle(imageListDouble);
+};
+
+export default useCardList;
