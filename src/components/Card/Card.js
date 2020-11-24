f1b40a103fb469494be722ece18f827dfc7ec974
Edward <edwardhhf0102@gmail.com>
2020-11-24 15:26:37 +0800

    refactor: to make the class name more pretty

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ import styles from './Card.module.scss';
 const Card = ({ imgSrc, isShowed, onClick }) => {
   return (
     <div
-      className={`${styles['card']} ${isShowed ? styles['show'] : ''}`}
+      className={`${styles['card']} ${isShowed ? styles['show'] : ''}`.trim()}
       onClick={onClick}
     >
       <img src={isShowed ? imgSrc : null} className={styles['img']} />
